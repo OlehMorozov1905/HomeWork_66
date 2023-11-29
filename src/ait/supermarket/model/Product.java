6de75330b30c3805d98b6d35edc1d86a73d8eb06
Oleh Morozov <olegmorozov140988@gmail.com>
2023-11-29 20:51:31 +0100

    Supermarket With Stream

---
+++ src/ait/supermarket/model/Product.java
@@ -0,0 +1,75 @@
+package ait.supermarket.model;
+
+import java.time.LocalDate;
+
+public class Product {
+    private long barCode;
+    private String name;
+    private String category;
+    private String brand;
+    private double price;
+    private LocalDate expDate;
+    public Product(long barCod, String name, String category, String brand, double price, LocalDate expDate) {
+        this.barCode = barCod;
+        this.name = name;
+        this.category = category;
+        this.brand = brand;
+        this.price = price;
+        this.expDate = expDate;
+    }
+
+    public long getBarCode() {
+        return barCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public LocalDate getExpDate() {
+        return expDate;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "barCod=" + barCode +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                ", expDate=" + expDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+
+        Product product = (Product) o;
+
+        return barCode == product.barCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (barCode ^ (barCode >>> 32));
+    }
+}
